c851c1a94f59dbfa26d1c6001af14d37a7b791fe
Charles Weber <charleseweber@gmail.com>
2018-09-30 03:05:04 -0400

    Small changes to integrate html with flask

--- Google_cloud/main.py
+++ Google_cloud/main.py
@@ -7,6 +7,7 @@ from google.appengine.api import app_identity
 
 app = Flask(__name__)
 
+# Index
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
@@ -17,9 +18,15 @@ def main():
             return 'No file selected.'
         if file:
             path = '/' + os.environ.get('BUCKET_NAME',app_identity.get_default_gcs_bucket_name()) + '/input/current.gpx'
-            gcs_file = gcs.open(path,'w',content_type='text/plain')
+            gcs_file = gcs.open(path,'w',content_type='application/gpx+xml')
             gcs_file.write(file.read())
             gcs_file.close()
-            return 'File upload success.'
+            return render_template('index.html',fileUploaded=True)
     else:
-        return render_template('index.html')
+        return render_template('index.html',fileUploaded=False)
+
+# Download
+@app.route('/download')
+def download():
+    expfile = 1 # GET FILE FROM CLOUD
+    return "DOWNLOAD FILE" #send_file(expfile, mimetype='application/gpx+xml', as_attachment=True, attachment_filename='GPSActivity')
