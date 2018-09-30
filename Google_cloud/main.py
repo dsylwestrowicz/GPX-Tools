c7403c0a9555452323bef147a4dd072205d697df
Charles Weber <charleseweber@gmail.com>
2018-09-30 02:31:19 -0400

    Completed cloudstorage upload python

--- Google_cloud/main.py
+++ Google_cloud/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import logging
 import os
 import cloudstorage as gcs
+import webapp2
 from google.appengine.api import app_identity
 
 app = Flask(__name__)
@@ -10,11 +12,14 @@ def main():
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'No file part.'
-        file = request.files.get('file')
+        file = request.files['file']
         if file.filename == '':
             return 'No file selected.'
         if file:
-            #public_url = storage.upload_file(file.read(),file.filename,file.content_type)
-            return 'Upload success: '# + public_url
+            path = '/' + os.environ.get('BUCKET_NAME',app_identity.get_default_gcs_bucket_name()) + '/input/current.gpx'
+            gcs_file = gcs.open(path,'w',content_type='text/plain')
+            gcs_file.write(file.read())
+            gcs_file.close()
+            return 'File upload success.'
     else:
         return render_template('index.html')
